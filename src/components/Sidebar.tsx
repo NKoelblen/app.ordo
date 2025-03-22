6b20e577e744a9c3771566e4a831bb3bf297a00e
NKoelblen <noemie.koelblen@icloud.com>
2025-03-22 10:51:36 +0100

    Set Spaces

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Drawer, Menu, MenuItem, IconButton, Box } from '@mui/material';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { useSpaces } from '../contexts/SpaceContext';
 import { Link } from 'react-router-dom';
 import '../styles/components/Sidebar.scss';
 
@@ -16,6 +17,12 @@ const Sidebar = () => {
 		setAnchorEl(null);
 	};
 
+	const { spaces } = useSpaces();
+
+	if (!Array.isArray(spaces)) {
+		return <div>Chargement...</div>;
+	}
+
 	return (
 		<Drawer
 			anchor="left"
@@ -29,12 +36,7 @@ const Sidebar = () => {
 				Ordo
 			</Link>
 
-			<Box
-				display="flex"
-				alignItems="center"
-				gap={2}
-				className="nav-item nav-title"
-			>
+			<Box className="nav-item nav-title">
 				{/* Titre */}
 				<span>Espaces</span>
 
@@ -62,6 +64,40 @@ const Sidebar = () => {
 					<MenuItem onClick={handleClose}>Item 2</MenuItem>
 				</Menu>
 			</Box>
+
+			<ul>
+				{spaces.map((space) => (
+					<li
+						key={space.id}
+						className="nav-item"
+					>
+						<span>{space.name}</span>
+						{/* Bouton du menu */}
+						<IconButton onClick={handleClick}>
+							<MoreHorizIcon />
+						</IconButton>
+
+						{/* Menu déroulant */}
+						<Menu
+							anchorEl={anchorEl}
+							open={open}
+							onClose={handleClose}
+							anchorOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							sx={{ mt: -0.75 }}
+						>
+							<MenuItem onClick={handleClose}>Item 1</MenuItem>
+							<MenuItem onClick={handleClose}>Item 2</MenuItem>
+						</Menu>
+					</li>
+				))}
+			</ul>
 		</Drawer>
 	);
 };
